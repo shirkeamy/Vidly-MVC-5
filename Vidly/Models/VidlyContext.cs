0298d884709a8cd82d074f2adf242c58331c4174
Amol Shirke <shirkeamy@gmail.com>
2022-03-18 17:50:08 +0530

    Remove old and add project with Authentiction

---
+++ Vidly/Models/VidlyContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class VidlyContext:DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+        public VidlyContext() : base("name=VidlyConnStrings")
+        {
+
+        }
+    }
+}
