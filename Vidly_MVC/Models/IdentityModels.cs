43bbf00d5dd085ef3bc4bed39bf25ff0e99bd9b6
Amol Shirke <shirkeamy@gmail.com>
2022-03-18 18:23:25 +0530

    Add Customer in migration and create table

--- Vidly_MVC/Models/IdentityModels.cs
+++ Vidly_MVC/Models/IdentityModels.cs
@@ -20,6 +20,9 @@ namespace Vidly_MVC.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+
+        public DbSet<Customer> Customers { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
