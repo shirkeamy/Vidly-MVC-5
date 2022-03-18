cc9c420a39633e325289e8778c910aedc15924f1
Amol Shirke <shirkeamy@gmail.com>
2022-03-18 17:00:21 +0530

    Add project files.

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            IEnumerable<Customer> customers = new List<Customer>
+            {
+                 new Customer{ Id=1,Name="Arjun"},
+                new Customer{ Id=2,Name="Karn"},
+                new Customer{ Id=3,Name="Krishna"},
+                new Customer{ Id=4,Name="Bhishma"},
+                new Customer{ Id=5,Name="Bhim"},
+                new Customer{ Id=6,Name="Nakul"},
+                new Customer{ Id=7,Name="Sahadev"},
+                new Customer{ Id=8,Name="Yudhisthir"},
+            };
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            IEnumerable<Customer> customers = new List<Customer>
+            {
+                 new Customer{ Id=1,Name="Arjun"},
+                new Customer{ Id=2,Name="Karn"},
+                new Customer{ Id=3,Name="Krishna"},
+                new Customer{ Id=4,Name="Bhishma"},
+                new Customer{ Id=5,Name="Bhim"},
+                new Customer{ Id=6,Name="Nakul"},
+                new Customer{ Id=7,Name="Sahadev"},
+                new Customer{ Id=8,Name="Yudhisthir"},
+            };
+
+            var customer = customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+            else
+                return View(customer);
+        }
+
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            IEnumerable<Movie> movies = new List<Movie> {
+                new Movie{ Id =1,Name = "Shrek!"},
+                new Movie{ Id =2,Name = "Wall-e"},
+                new Movie{ Id =3,Name = "Avengers"},
+                new Movie{ Id =4,Name = "Spider-Man"},
+            };
+
+            return View(movies);
+
+        }
+
+        #region Practice
+        //public ActionResult Random()
+        //{
+        //    var movie = new Movie() { Name = "Shrek!" };
+        //    //ViewData["Movie"] = movie;
+        //    //ViewBag.Movie= movie;
+        //    var customers = new List<Customer>
+        //    {
+        //        new Customer{ Name="Amol Shirke"},
+        //        new Customer{ Name="Komal Shirke"},
+        //        new Customer{ Name="Komal Amol"}
+        //    };
+        //    var randomMovies = new RandomMovieViewModel
+        //    {
+        //        Movie=movie,
+        //        Customers=customers
+        //    };
+        //    return View(randomMovies);
+        //}
+        //public ActionResult Edit(int id)
+        //{
+        //    return Content("id= " + id);
+        //}
+
+
+        //[Route("movies/released/{year}/{month:regex(\\d(2)):range(1,12)}")]
+        //public ActionResult RealeasedByYear(int year, int month)
+        //{
+        //    return Content(year + "/" + month);
+        //}
+
+        #endregion Practice
+
+    }
+}
