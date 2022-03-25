0a2c8cd67582b422bc9874aa0b5318c255a5eaca
Amol Shirke <shirkeamy@gmail.com>
2022-03-25 13:29:38 +0530

    Add Data Transfer Object for Customer

---
+++ Vidly_MVC/Dtos/CustomerDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly_MVC.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribeToNewLeter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
